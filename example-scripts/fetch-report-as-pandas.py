4af94d780fc53556ec2b988342c37bd4250efd32
Ghos_h <509036+soumyabrataghosh@users.noreply.github.com>
2020-07-04 00:45:26 +0200

    Create fetch-report-as-pandas.py

---
+++ example-scripts/fetch-report-as-pandas.py
@@ -0,0 +1,17 @@
+import requests
+from io import StringIO
+import pandas as pd
+
+fields = {
+    'token': 'REDCAPSECRETTOKEN',
+    'content': 'report',
+    'format': 'csv',
+    'report_id': '5', # Get report_id from REDCap web frontend
+    'csvDelimiter': '',
+    'rawOrLabel': 'label',
+    'rawOrLabelHeaders': 'label',
+}
+
+r = requests.post('https://redcap-api-end-point-url', fields)
+pr = StringIO(r.text)
+df = pd.read_csv(pr, header=0, squeeze=True).T.to_dict()
